c4c95dcde128b36c8d80169209f44c1622dde024
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-02-26 16:29:55 +0530

    Add files via upload

---
+++ 02Basics/02Variable1.js
@@ -0,0 +1,27 @@
+const uid = 229302092;
+
+
+ var lastName = "Joshi";
+
+ var firstName = "Rishi";
+
+ var email = "rishijoshi.outlook@gmail.com";
+
+ var state = "Rajasthan";
+ 
+ var country ="India";
+
+ var isLoggedInFromGoogle = false;
+
+ console.log(`
+        With UID : ${uid}
+        Last Name : ${lastName}
+        First Name : ${firstName}
+        Registered Email : ${email}
+        From State : ${state}
+        From Country : ${country}
+        Login from Google : ${isLoggedInFromGoogle}
+ 
+ `);
+ 
+
